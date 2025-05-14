1fcb874281055f96ff293288e87d2270f9f84ff3
Chris Hampu <champu@riskthinking.ai>
2025-05-14 20:53:27 +0300

    Add docstrings for the new climate functions

--- velo_sdk/api/climate.py
+++ velo_sdk/api/climate.py
@@ -6,9 +6,15 @@ class Climate:
         self.client = client
 
     def list_horizons(self) -> list[int]:
+        """
+        List the available horizons for climate analysis.
+        """
         return list(range(2025, 2100, 5))
 
     def list_pathways(self) -> list[str]:
+        """
+        List the available pathways for climate analysis.
+        """
         return [
             "SV",
             "ssp126",
